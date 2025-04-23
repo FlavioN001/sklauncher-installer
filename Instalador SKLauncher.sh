698b0f875814f35d8dc57d665521de81a633b31a
Flerovium <101293347+FlavioN001@users.noreply.github.com>
2025-04-22 21:48:16 -0300

    Add files via upload

---
+++ Instalador SKLauncher.sh
@@ -0,0 +1,15 @@
+mkdir ~/.sklauncher
+mkdir ~/.sklauncher/temp
+git clone https://github.com/FlavioN001/sklauncher-installer/ ~/.sklauncher/temp
+cd ~/.sklauncher/temp
+sudo chmod +x SKlauncher-*
+cp SKlauncher-* ../
+cp SKlauncher.desktop ~/.local/share/applications
+cd ..
+rm temp
+
+echo
+echo
+echo "SKlauncher instalado com sucesso!"
+echo
+echo
